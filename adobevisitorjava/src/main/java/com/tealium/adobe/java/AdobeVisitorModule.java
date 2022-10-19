6bdfd931a0b34875fa7be4097359d9c2903a80c4
Karen Tamayo <karentamayo@Karens-MBP.webpass.net>
2022-10-19 07:55:42 -0700

    Add sessionToken + update tealium version

--- adobevisitorjava/src/main/java/com/tealium/adobe/java/AdobeVisitorModule.java
+++ adobevisitorjava/src/main/java/com/tealium/adobe/java/AdobeVisitorModule.java
@@ -29,7 +29,6 @@ import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
